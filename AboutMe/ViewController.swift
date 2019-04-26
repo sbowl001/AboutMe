5b6c3dce25b5bede27238b7b9b1caeb459545d28
Stephanie Bowles <sbowl001@fiu.edu>
2019-04-26 10:06:14 +0800

    Print name at Launch

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
          print("Hi my name is Stephanie Bowles")
+         print("I like cookies")
     }
 
 
