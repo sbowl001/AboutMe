c430394fbf8176d8adeb5b1d5a7ff44351156e8c
Stephanie Bowles <sbowl001@fiu.edu>
2019-04-26 09:59:53 +0800

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Stephanie Bowles on 4/26/19.
+//  Copyright © 2019 Stephanie Bowles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         print("Hi my name is Stephanie Bowles")
+    }
+
+
+}
+
